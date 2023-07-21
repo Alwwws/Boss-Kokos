bdbd9b97f1a4ffe04f33a93234e2a4415234d8bf
Vadym Konduforov <vadim.konduforov123@gmail.com>
2023-07-21 18:10:51 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+var mixer = mixitup('.footer__bank-all', {
+  load: {
+    filter: '.category-title'
+  }
+});
+
+emailInput.onclick = () => {
+  navigator.clipboard.writeText('olga.fortuna210@gmail.com')
+}
+emailInput1.onclick = () => {
+  navigator.clipboard.writeText('4441 1144 6748 6724')
+}
+emailInput2.onclick = () => {
+  navigator.clipboard.writeText('5167 8020 0139 2320')
+}
+emailInput3.onclick = () => {
+  navigator.clipboard.writeText('UA74 3808 0500 0000 0026 0087 7439 2')
+}
+emailInput4.onclick = () => {
+  navigator.clipboard.writeText('UA17 3808 0500 0000 0026 0097 7442 1')
+}
+emailInput5.onclick = () => {
+  navigator.clipboard.writeText('UA36 3808 0500 0000 0026 0007 7441 9')
+}
+
+$(function () {
+  $('.top-slider').slick({
+    dots: true,
+    arrows: true,
+  });
+})
